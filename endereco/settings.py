84d305100c6ba84bafa7b3f1ac772d6712197815
ismairdrs <ismairdrs@outlook.com>
2021-04-04 02:02:33 -0300

    configurando o app fix

--- endereco/settings.py
+++ endereco/settings.py
@@ -14,7 +14,7 @@ default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
 DATABASES = { 'default': config('DATABASE_URL', default=default_dburl, cast=dburl), }
 
 
-ALLOWED_HOSTS = ['endereco-api-pizzaria']
+ALLOWED_HOSTS = ['endereco-api-pizzaria.herokuapp.com']
 
 
 # Application definition
