3ed95687db0e6581464a7a75972807388d66ea80
ismairdrs <ismairdrs@outlook.com>
2021-04-22 21:50:48 -0300

    fix

--- core/api/v1/views/endereco.py
+++ core/api/v1/views/endereco.py
@@ -3,24 +3,25 @@ from rest_framework.exceptions import ValidationError
 import django_filters.rest_framework
 from core.api.v1.serializer import EnderecoSerializer
 from core.models import Endereco
-
+from django.shortcuts import get_object_or_404
 
 class EnderecViewSet(viewsets.ModelViewSet):
-   # queryset = Endereco.objects.all()
     serializer_class = EnderecoSerializer
     filter_backends = [django_filters.rest_framework.DjangoFilterBackend]
-    #filterset_fields = ['usuario',]
 
     def get_queryset(self):
-
-        user = self.request.query_params.get('usuario')
-        if user:
-            try:
-                queryset = Endereco.objects.filter(usuario=str(user))
-            except:
-                content = {'error': 'ID inválido'}
-                raise ValidationError(content)
+        usuario = self.request.query_params.get('usuario')
+        if usuario:
+            queryset = self.buscar_endereco_usuario(usuario)
         else:
             content = {'Requisição inválida': 'ID do usuário é obrigatório no query params /?usuario=id'}
+            raise ValidationError(content)
+        return queryset
+
+    def buscar_endereco_usuario(self, usuario):
+        try:
+            queryset = Endereco.objects.filter(usuario=usuario)
+        except:
+            content = {'error': 'ID inválido'}
             raise ValidationError(content)
         return queryset
